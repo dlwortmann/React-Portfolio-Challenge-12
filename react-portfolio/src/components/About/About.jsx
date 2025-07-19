41344514283590572d85c5edda630bc7ef602568
dlwortmann <dannywortmann@gmail.com>
2025-07-19 18:46:54 -0500

    remove repeated text in about section

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -6,7 +6,7 @@ function About() {
     <div className="about-container">
       <img src="./../profilephoto.jpeg" alt="Photo of Danny" className="profile-photo" />
       <p className="bio">
-        Motivated junior full stack developer transitioning from 15 years of leadership in the hospitality industry. Recently completed a full stack web development bootcamp through the University of Kansas. Adept at team management, problem-solving, and delivering high-quality service—now applying those strengths to building modern, scalable web applications using React, GraphQL, and JavaScript. Motivated junior full stack developer transitioning from 15 years of leadership in the hospitality industry. Recently completed a full stack web development bootcamp through the University of Kansas. Adept at team management, problem-solving, and delivering high-quality service—now applying those strengths to building modern, scalable web applications using React, GraphQL, and JavaScript.
+        Motivated junior full stack developer transitioning from 15 years of leadership in the hospitality industry. Recently completed a full stack web development bootcamp through the University of Kansas. Adept at team management, problem-solving, and delivering high-quality service—now applying those strengths to building modern, scalable web applications using React, GraphQL, and JavaScript. 
       </p>
     </div>
   )
